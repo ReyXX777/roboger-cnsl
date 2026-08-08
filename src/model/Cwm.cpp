d3279139bebce1c50d42047f4da78090a7292f0f
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-08 13:50:40 +0530

    Initialize with additional members

--- src/model/Cwm.cpp
+++ src/model/Cwm.cpp
@@ -1,4 +1,10 @@
 #include "Cwm.h"
 
 ConsoleWorkflowModel::ConsoleWorkflowModel(QObject *parent)
-    : QObject(parent) {}
+    : QObject(parent)
+    , m_currentState(State::AccountSelect)
+    , m_selectedSurgeon("GUEST")
+    , m_pinProgress(0)
+    , m_ergoProgress(0.0)
+    , m_monopolarCut(6)
+    , m_bipolarCoag(3) {}
