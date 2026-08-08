cde151ccd355c6f6f81146b3759836d335e14803
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-08 13:55:14 +0530

    clnp
    Removed unnecessary blank lines for cleaner code.

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,23 @@
 #include "controller/Wsc.h"
 
 int main(int argc, char *argv[]) {
-    
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
 
-    
     ConsoleWorkflowModel workflowModel;
 
-    
     WorkflowSimulationController simController;
     simController.setModel(&workflowModel);
 
-    
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("consoleModel", &workflowModel);
 
-    
-    
-    
     engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
     
     if (engine.rootObjects().isEmpty())
         return -1;
 
-    
     simController.startSimulation();
 
     return app.exec();
