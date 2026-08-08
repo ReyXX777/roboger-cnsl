20fc30c7977093022b179245590a1fea5238a36b
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-08 13:39:09 +0530

    list initial rfr

--- src/controller/Wsc.cpp
+++ src/controller/Wsc.cpp
@@ -1,7 +1,10 @@
 #include "Wsc.h"
 
 WorkflowSimulationController::WorkflowSimulationController(QObject *parent)
-    : QObject(parent) {
+    : QObject(parent)
+    , m_timer()
+    , m_model(nullptr)
+    , m_simTime(0.0) {
     connect(&m_timer, &QTimer::timeout, this, &WorkflowSimulationController::onSimulationTick);
 }
 
@@ -14,7 +17,7 @@ void WorkflowSimulationController::setModel(ConsoleWorkflowModel *model) {
 }
 
 void WorkflowSimulationController::startSimulation() {
-    m_timer.start(16); // ~60fps delta-time loop
+    m_timer.start(16);
 }
 
 void WorkflowSimulationController::onSimulationTick() {
@@ -23,75 +26,57 @@ void WorkflowSimulationController::onSimulationTick() {
     double dt = m_timer.interval() / 1000.0;
     m_simTime += dt;
 
-    
     if (m_model->currentState() == ConsoleWorkflowModel::AccountSelect) {
-        
         if (m_simTime > 3.0) {
             m_model->setSelectedSurgeon("SUPERVISOR");
-            m_model->setPinProgress(0); // Reset PIN dots
-            m_model->setErgoProgress(0.0); // Reset ergonomics for fresh loop
+            m_model->setPinProgress(0);
+            m_model->setErgoProgress(0.0);
             m_model->setCurrentState(ConsoleWorkflowModel::PinEntry);
             m_simTime = 0.0;
         }
     } 
-    
     else if (m_model->currentState() == ConsoleWorkflowModel::PinEntry) {
-        
         int targetPins = static_cast<int>(m_simTime / 0.4); 
         
         if (targetPins <= 6) {
             m_model->setPinProgress(targetPins);
         }
 
-        
         if (m_model->pinProgress() >= 6 && m_simTime > 3.4) {
             m_model->setCurrentState(ConsoleWorkflowModel::Ergonomics);
             m_simTime = 0.0;
         }
     }
-    
     else if (m_model->currentState() == ConsoleWorkflowModel::Ergonomics) {
-        
         double targetProgress = qMin(1.0, m_simTime / 4.0);
         m_model->setErgoProgress(targetProgress);
 
-        
         if (m_simTime > 5.5) {
             m_model->setCurrentState(ConsoleWorkflowModel::ProcedureSelect);
             m_simTime = 0.0;
         }
     }
-    
     else if (m_model->currentState() == ConsoleWorkflowModel::ProcedureSelect) {
-        
         if (m_simTime > 2.5) {
             m_model->setCurrentState(ConsoleWorkflowModel::EnergyConfig);
             m_simTime = 0.0;
         }
     }
-    
     else if (m_model->currentState() == ConsoleWorkflowModel::EnergyConfig) {
-        
-        
-        
         if (m_simTime > 1.0 && m_model->monopolarCut() != 5) {
             m_model->setMonopolarCut(5);
         }
-        
         
         if (m_simTime > 2.0 && m_model->bipolarCoag() != 4) {
             m_model->setBipolarCoag(4);
         }
 
-        
         if (m_simTime > 3.5) {
             m_model->setCurrentState(ConsoleWorkflowModel::Overview);
             m_simTime = 0.0;
         }
     }
-    
     else if (m_model->currentState() == ConsoleWorkflowModel::Overview) {
-        
         if (m_simTime > 5.0) {
             m_model->setPinProgress(0);
             m_model->setErgoProgress(0.0);
