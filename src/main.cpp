9c0974050f08ae7072ed9eb3319fe88cc8fc01c5
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-08 19:50:03 +0530

    rfr2.4

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <cstdlib>
+
 #include "model/Cwm.h"
 #include "controller/Wsc.h"
 
 int main(int argc, char *argv[]) {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QGuiApplication app(argc, argv);
 
@@ -17,10 +21,12 @@ int main(int argc, char *argv[]) {
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("consoleModel", &workflowModel);
 
-    engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
-    
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    const QUrl url(QStringLiteral("qrc:/qml/Main.qml"));
+    engine.load(url);
+
+    if (engine.rootObjects().isEmpty()) {
+        return EXIT_FAILURE;
+    }
 
     simController.startSimulation();
 
